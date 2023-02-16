27ce82399e3bef197c06c4ad1d769e61f00b6777
WereAM <michelle.ambunya@strathmore.edu>
2023-02-16 12:28:48 +0300

    Landing page
    added links to other pages
    
    linked to home page and user page
    
    added navbar
    
    changed font size and positioning of elements
    
    increased font size, incerased login button size and added padding to footer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import HomePage from './home';
+import UserPage from './user';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/home",
+    element: <HomePage/>,
+  },
+  {
+    path: "/user",
+    element: <UserPage/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
---
+++ src/landing.js
@@ -0,0 +1,24 @@
+export default function LandingPage (){
+    return (
+        <>
+        <div className="landing-content">
+            <h>This app fetches data <br /> from a JSON placeholder <br /> and displays to users</h>
+            <img src="https://m.media-amazon.com/images/I/71eOgACsdQL._AC_SL1500_.jpg"  alt=""/>
+            <p>Login to view content.</p>
+            <button>LOGIN</button>
+        </div>
+
+        {/* <div>
+            <img src="https://m.media-amazon.com/images/I/71eOgACsdQL._AC_SL1500_.jpg"  alt=""/>
+        </div> */}
+        
+        
+        <footer>
+            <a href="/">CONTACTS</a>
+            <p>Author: Michelle Were</p>
+            <p>Mail to : thisproject@gmail.com.com</p>
+            <p>Phone: +1 234-566-456</p>
+        </footer>
+        </>
+    )
+}
