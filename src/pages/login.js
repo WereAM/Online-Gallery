9f5b02a9de650c7668cbbd327544bddd2e1bafbd
WereAM <michelle.ambunya@strathmore.edu>
2023-03-13 11:50:56 +0300

    Updated logo, footer and design for landing page and login page
    Updated design for landing page and login pages

--- src/pages/login.js
+++ src/pages/login.js
@@ -10,14 +10,12 @@ export default function LoginPage ({Login, error}){
     }
 
     return(
-        
-        <div className='form'>
-            <div className="login">
+        <div className="login">
 
-                <div className="userIcon">
-                    <img src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" alt="" width='200' height='200'></img>
-                </div>
-
+            <div className="userIcon">
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" alt="" width='200' height='200'></img>
+            </div>
+            
             <form className="loginForm" onSubmit={handleSubmit}>
                 <label> Name: </label>
                 <br/>
@@ -27,7 +25,7 @@ export default function LoginPage ({Login, error}){
                     placeholder="Enter your username"
                     onChange={event => setDetails({...details, name: event.target.value})}
                     value={details.name}
-                    />
+                />
                 <br/>
 
                 <label>Password: </label>
@@ -38,15 +36,14 @@ export default function LoginPage ({Login, error}){
                     placeholder="Enter your password"
                     onChange={event => setDetails({...details, password: event.target.value})}
                     value={details.password}
-                    />
+                />
                 <br/>
 
-                <input className="loginBtn" type="submit" value="Sign In" />
+                <button className="loginBtn" type="submit">Sign In</button>
 
                 <p>{(error!=='')  ? (<div className='error'>{error}</div>) : ''} </p>
-            
+        
             </form> 
-            </div>
         </div>
     );
 }
